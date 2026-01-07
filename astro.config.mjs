f01c07c69f0b311848c894aec619831510c3700a
magocar22 <mj.aguilargomez@outlook.com>
2026-01-07 17:58:53 +0100

    Pequeñas correcciones. Favicon. CookieBanner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-  integrations: [tailwind()]
+  // 1. AÑADE ESTA LÍNEA CON TU DOMINIO REAL (O EL PROVISIONAL)
+  site: 'https://tuprimerhogar.es',
+
+  integrations: [tailwind(), sitemap()]
 });
